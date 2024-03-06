e34b92d9d26273a3c1e7184cab23aedb2e32603b
Amit Jangu <amit.scorpio42@gmail.com>
2024-03-06 17:19:13 +0530

    REFACTOR: Cleaned up sessions controller, Added unconfirmed user alert

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,10 +7,14 @@ class SessionsController < ApplicationController
 
   def create
     # Only authenticate users with confirmed accounts
-    if user = User.confirmed.authenticate_by(email: params[:email], password: params[:password])
-      sign_in(user)
-      auth_notice = user.mfa_enabled? ? t(".proceed_with_mfa") : t(".success")
-      redirect_to root_path, notice: auth_notice
+    if user = User.authenticate_by(email: params[:email], password: params[:password])
+      if user.confirmed?
+        sign_in(user)
+        redirect_to root_path, notice: sign_in_notice_for(user)
+      else
+        flash.now[:alert] = t(".unconfirmed")
+        render :new, status: :unprocessable_entity
+      end
     else
       flash.now[:alert] = t(".failure")
       render :new, status: :unprocessable_entity
@@ -20,5 +24,11 @@ class SessionsController < ApplicationController
   def destroy
     sign_out
     redirect_to new_session_path, notice: t(".success")
+  end
+
+  private
+
+  def sign_in_notice_for(user)
+    user.mfa_enabled? ? t(".proceed_with_mfa") : t(".success")
   end
 end
